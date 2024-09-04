179582ec7522797e69eb16e3094c3359279e5793
sumitraiyer <43791429+sumitraiyer@users.noreply.github.com>
2024-09-04 17:18:39 +0530

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+streamlit_code = """
+import streamlit as st
+import pandas as pd
+st.title("VTEC forecasting")
+
+# CHOOSE SLIDING/ TUMBLING WINDOW
+# radio button to choose height format
+status = st.radio('Select your window type: ',
+                  ('sliding', 'tumbling'))
+
+import csv
+with open('threeday vtec_Bang.csv', mode ='r')as file:
+  csvFile = csv.reader(file)
+  for lines in csvFile:
+        print(lines)
+
+
+df = pd.read_csv('threeday vtec_Bang.csv')
+df.to_csv('data1.csv')
+
+        
+"""
+
+# Save the Streamlit app to a file
+with open("app.py", "w") as f:
+    f.write(streamlit_code)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
